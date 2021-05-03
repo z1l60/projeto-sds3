bee05aa4cba52d6db289ce82efe0f1a22f0722d6
z1l60 <zildoo.c@gmail.com>
2021-05-03 20:28:55 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/zildo-cesar-borcathe-6bbb22160/" target="_blank" rel="noreferrer">Zildo Cesar Borcathe</a></p>
+                <p className="text-light"><small><strong>Semana Spring React 3</strong><br />
+            Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
