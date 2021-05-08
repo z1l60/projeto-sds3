211346a1dcb96e0aa27effe471eac12e11bd5b34
z1l60 <zildoo.c@gmail.com>
2021-05-08 10:03:46 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,8 @@
-import NavBar from "components/NavBar";
-import Footer from "components/Footer";
-import DataTable from "components/DataTable";
-import BarCharts from "components/BarCharts";
-import DonutsCharts from "components/DonutsCharts";
+import Routes from "Routes";
 
 function App() {
   return (
-    <>
-      <NavBar />
-      <div className="container">
-        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
-
-        <div className="row px-3" >
-          <div className="col-sm-6" >
-          <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
-            <BarCharts />
-          </div>
-          <div className="col-sm-6" >
-          <h5 className="text-center text-secondary">Total de Vendas</h5>
-            <DonutsCharts />
-          </div>
-        </div>
-
-        <div className="py-3" >
-          <h2 className="text-primary" >Total de vendas</h2>
-        </div>
-        <DataTable />
-      </div>
-      <Footer />
-    </>
+    <Routes />
   );
 }
 
